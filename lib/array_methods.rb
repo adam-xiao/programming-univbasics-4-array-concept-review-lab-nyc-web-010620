6bee0b0cd135e2ce3f16c9864401c21e1bf06bd7
Adam Xiao <adam.x.xiao@gmail.com>
2019-12-26 00:35:51 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,10 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  array.length.times do |index|
+    array[index] == value_to_find
+    return index
+  end
+  nil
+  #array.index(value_to_find)
 end
 
 def find_max_value(array)
